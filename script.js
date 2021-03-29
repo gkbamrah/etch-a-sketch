59878a68bc3d26d093c2e5ffb5491cfc4a55163e
gkbamrah <guramritb@gmail.com>
2021-03-28 22:42:14 -0600

    etch-a-sketch functionality is present

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function generateGrid(dimensions) {
+    if(document.getElementsByClassName("row").length > 0) {
+        let rows = Array.prototype.slice.call(document.getElementsByClassName("row"));
+        for(let row of rows) {
+            row.remove();
+        }
+    }
     let container = document.getElementById("grid-container");
     for(let i = 0; i < dimensions; i++) {
         let row = document.createElement("div");
@@ -6,12 +12,27 @@ function generateGrid(dimensions) {
 
         for(let j = 0; j < dimensions; j++) {
             let square = document.createElement("div");
+            square.addEventListener("mouseover", (e) => {
+                e.target.classList.add("visited");
+            });
             square.classList.add("grid-square");
             row.appendChild(square);
         }
         
         container.appendChild(row);
     }
+}
+
+function handleOnClick() {
+    let response = prompt("How many squares per side?");
+        if(response != null) {
+            if(response > 100) {
+                alert("Please select a smaller number");
+            } else {
+                //console.log()
+                generateGrid(parseInt(response, 10));
+            }
+        }
 }
 
 function main() {
